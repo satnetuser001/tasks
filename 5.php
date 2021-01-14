876b4ae3acb81cbd259e40860ee8ac3b2846aa93
satnetuser001 <76094506+satnetuser001@users.noreply.github.com>
2021-01-14 16:24:44 +0200

    Update 5.php
    В процессе...

--- 5.php
+++ 5.php
@@ -6,27 +6,71 @@
 class Player{
 	public $Number;
 	public $Name;
+	public $Status = 'Active';
 
-	public function __construct($Number){
+	function __construct($Number){
 		$this -> Number = $Number;
 		$this -> Name = readline("Введите имя игрока № $this->Number : ");
 		echo "Игрок № $this->Number с именем $this->Name\n";
 	}
 }
 //$obj = new Player(1);
+/*
+Class Tour{
 
+}
+
+Class Winer{
+
+}
+*/
 class Game{
 	public $NumberOfPlayers;
-	public $ArrObj = Array();
+	public $ArrObjPlayers = array();
+	public $ArrWords = array();
 
-	public function __construct(){
+	function __construct(){
+		//Создаем массив игроков
 		$this->NumberOfPlayers = readline("Введите количество играков: ");
 		for ($i=1; $i <= $this->NumberOfPlayers; $i++) { 
-			$this->ArrObj["Player № $i"] = new Player($i);
+			$this->ArrObjPlayers["Player$i"] = new Player($i);
 		}
+		/*
+		//начинаем игру
+		do {
+			# Tour
+		} while (нет победителя);
+		*/
 	}
 }
 
 $objGame = new Game();
-print_r($objGame->ArrObj);
+print_r($objGame->ArrObjPlayers);
+
+/*массив игроков
+Array
+(
+    [Player1] => Player Object
+        (
+            [Number] => 1
+            [Name] => Саша
+            [Status] => Active
+        )
+
+    [Player2] => Player Object
+        (
+            [Number] => 2
+            [Name] => Маша
+            [Status] => Active
+        )
+
+    [Player3] => Player Object
+        (
+            [Number] => 3
+            [Name] => Коля
+            [Status] => Active
+        )
+
+)
+*/
 ?>
